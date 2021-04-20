d61dac7ea008e3230f0c8f8f52a0b7a7648de196
agiappichini <a.giappichini@pucciufficio.com>
2021-04-20 11:18:47 +0100

    modifica print

--- src/gitesercizio/GitEsercizio.java
+++ src/gitesercizio/GitEsercizio.java
@@ -16,6 +16,7 @@ public class GitEsercizio {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Modifica");
     }
     
 }
